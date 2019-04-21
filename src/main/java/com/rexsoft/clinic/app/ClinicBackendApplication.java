3b84f375194c6b8cdc247d1ed258701203b53ee3
Diego Alejandro <diego.fc.1@hotmail.com>
2019-04-20 22:19:19 -0500

    first commit

---
+++ src/main/java/com/rexsoft/clinic/app/ClinicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rexsoft.clinic.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicBackendApplication.class, args);
+	}
+
+}
